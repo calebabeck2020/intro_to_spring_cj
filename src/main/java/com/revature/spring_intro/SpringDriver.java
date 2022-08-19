712fc40ac9911f91807d43b0639264d2e73e5cee
calebabeck2020 <calebabeck@gmail.com>
2022-08-19 11:40:50 -0400

    first commit

--- src/main/java/com/revature/spring_intro/SpringDriver.java
+++ src/main/java/com/revature/spring_intro/SpringDriver.java
@@ -4,54 +4,62 @@ import com.revature.spring_intro.models.BaseballCoach;
 import com.revature.spring_intro.models.FootballCoach;
 import com.revature.spring_intro.models.TrackCoach;
 import com.revature.spring_intro.services.MotivationService;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 public class SpringDriver {
 
     public static void main(String[] args) {
 
-        MotivationService motivationService = new MotivationService();
+        try (ClassPathXmlApplicationContext beanContainer = new ClassPathXmlApplicationContext("beans.xml")) {
 
-        //
-        TrackCoach trackCoach = new TrackCoach(motivationService);
-        BaseballCoach baseballCoach = new BaseballCoach(motivationService);
+            System.out.println("Bean Container Created...");
 
-        // Construct a new football coach and set the service made above
-        FootballCoach footballCoach = new FootballCoach();
-        footballCoach.setMotivationService(motivationService);
+            MotivationService motivationService = new MotivationService();
+
+            //
+            TrackCoach trackCoach = beanContainer.getBean("trackCoach",TrackCoach.class);
+            BaseballCoach baseballCoach = beanContainer.getBean("baseballCoach",BaseballCoach.class);
+
+            // Construct a new football coach and set the service made above
+            FootballCoach footballCoach = new FootballCoach();
+            footballCoach.setMotivationService(motivationService);
 
 
-        // can now run the commands for each coach
-        System.out.println("***Begin look at our information in memory: Track****");
-        System.out.println();
+            // can now run the commands for each coach
+            System.out.println("***Begin look at our information in memory: Track****");
+            System.out.println();
 
-        System.out.println(trackCoach);
-        System.out.println(trackCoach.getMotivation());
-        System.out.println(trackCoach.getDailyWorkout());
+            System.out.println(trackCoach);
+            System.out.println(trackCoach.getMotivation());
+            System.out.println(trackCoach.getDailyWorkout());
 
-        System.out.println("********************************************************");
-        System.out.println();
+            System.out.println("********************************************************");
+            System.out.println();
 
-        System.out.println("***Begin look at our information in memory: Baseball****");
-        System.out.println();
+            System.out.println("***Begin look at our information in memory: Baseball****");
+            System.out.println();
 
-        System.out.println(baseballCoach);
-        System.out.println(baseballCoach.getMotivation());
-        System.out.println(baseballCoach.getDailyWorkout());
-        System.out.println("********************************************************");
-        System.out.println();
+            System.out.println(baseballCoach);
+            System.out.println(baseballCoach.getMotivation());
+            System.out.println(baseballCoach.getDailyWorkout());
+            System.out.println("********************************************************");
+            System.out.println();
 
-        System.out.println("***Begin look at our information in memory: Football****");
-        System.out.println();
+            System.out.println("***Begin look at our information in memory: Football****");
+            System.out.println();
 
-        System.out.println(footballCoach);
-        System.out.println(footballCoach.getMotivation());
-        System.out.println(footballCoach.getDailyWorkout());
-        System.out.println("Team name: " + footballCoach.getTeamName());
-        System.out.println("Email: " + footballCoach.getEmail());
+            System.out.println(footballCoach);
+            System.out.println(footballCoach.getMotivation());
+            System.out.println(footballCoach.getDailyWorkout());
+            System.out.println("Team name: " + footballCoach.getTeamName());
+            System.out.println("Email: " + footballCoach.getEmail());
 
-        System.out.println("********************************************************");
-        System.out.println();
+            System.out.println("********************************************************");
+            System.out.println();
 
+        } catch (Exception e) {
+
+        }
 
 
     }
